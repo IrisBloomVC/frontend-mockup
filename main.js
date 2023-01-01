123094469fe72da24a01d097f3a9e9d605383d3d
xFlest <mail@fle.st>
2023-01-02 05:34:40 +0900

    create message view

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 const toggleSidemenu = () => {
-  const target = document.getElementsByClassName('sidemenu')[0];
-  target.classList.toggle('sidemenu--open');
+  const target = document.getElementsByClassName('sidemenu')[0]
+  target.classList.toggle('sidemenu--open')
+}
+const resizeTextarea = (textarea) => {
+  textarea.style.height = 0
+  textarea.style.height = textarea.scrollHeight + "px"
+}
+const sendMessage = () => {
+  const textarea = document.getElementsByClassName('form__input')[0]
+  const messages = document.getElementsByClassName('content__messages')[0]
+  const lastMessage = messages.lastElementChild
+  const template = lastMessage.classList.contains('message--own')
+      ? document.getElementById('messageText__template').content.cloneNode(true)
+      : document.getElementById('message__template').content.cloneNode(true)
+  template.querySelector('.message__text').textContent = textarea.value
+  if (lastMessage.classList.contains('message--own')) {
+    lastMessage.appendChild(template)
+  } else {
+    messages.appendChild(template)
+  }
+  textarea.value = null
+  resizeTextarea(textarea)
 }
